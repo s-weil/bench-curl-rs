bed75177c9e7222814037e4bc1ab7959d1d963f1
snusnusnu <35423703+snusnusnu@users.noreply.github.com>
2022-12-27 17:23:54 +0100

    collect samples on different threads

--- burl/src/lib.rs
+++ burl/src/lib.rs
@@ -4,6 +4,7 @@ mod request_factory;
 mod stats;
 
 pub use config::BenchConfig;
+use config::DurationScale;
 pub use plots::plot_stats;
 
 use crate::config::ConcurrenyLevel;
@@ -12,6 +13,42 @@ use request_factory::RequestFactory;
 use reqwest::*;
 use stats::{Stats, StatsCollector};
 use std::time::Instant;
+
+fn timed_request(
+    timer: Instant,
+    request: &blocking::RequestBuilder,
+    stats_collector: &mut StatsCollector,
+) {
+    let request = request.try_clone().unwrap();
+    let measurement_start = timer.elapsed();
+    let start = Instant::now();
+
+    match request.send() {
+        Ok(response) => {
+            // TODO: better way of measuring the time?
+            let duration = start.elapsed();
+            let measurement_end = timer.elapsed();
+            stats_collector.add(measurement_start, measurement_end, duration, response);
+        }
+        Err(error) => {
+            error!("Error during sending request: {:?}", error);
+        }
+    }
+}
+
+fn collect_samples(
+    request_builder: blocking::RequestBuilder,
+    n_runs: usize,
+    duration_scale: DurationScale,
+) -> StatsCollector {
+    let timer = Instant::now();
+    let mut stats_collector = StatsCollector::init(n_runs, duration_scale);
+
+    for _ in 0..n_runs {
+        timed_request(timer, &request_builder, &mut stats_collector);
+    }
+    stats_collector
+}
 
 pub struct BenchClient {
     request_factory: RequestFactory,
@@ -30,36 +67,12 @@ impl BenchClient {
         })
     }
 
-    fn timed_request(
-        &self,
-        timer: &Instant,
-        request: &blocking::RequestBuilder,
-        stats_collector: &mut StatsCollector,
-    ) {
-        let request = request.try_clone().unwrap();
-        let measurement_start = timer.elapsed();
-        let start = Instant::now();
-
-        match request.send() {
-            Ok(response) => {
-                // TODO: better way of measuring the time?
-                let duration = start.elapsed();
-                let measurement_end = timer.elapsed();
-                stats_collector.add(measurement_start, measurement_end, duration, response);
-            }
-            Err(error) => {
-                error!("Error during sending request: {:?}", error);
-            }
-        }
-    }
-
     pub fn start_run(&self) -> Option<Stats> {
         let du = self.config.duration_unit();
 
         let n_runs = self.config.n_runs();
-        let mut stats_collector = StatsCollector::init(n_runs, du);
 
-        let request = match self.request_factory.assemble_request(&self.config) {
+        let request_builder = match self.request_factory.assemble_request(&self.config) {
             Some(req) => req,
             None => {
                 error!("Failed to compile the request");
@@ -67,30 +80,54 @@ impl BenchClient {
             }
         };
 
-        match self.config.concurrency_level() {
-            ConcurrenyLevel::Sequential => {
-                // Trigger un-timed requests, possibly to populate a cache or similiar
-                info!("Warming up");
-                for _ in 0..self.config.warmup_runs() {
-                    if let Err(error) = request.try_clone().unwrap().send() {
-                        error!("Warm up failed: {:?}", error);
-                        return None;
-                    }
-                }
-                info!(
-                    "Starting measurement of {} samples from {}",
-                    n_runs, self.config.url
-                );
-                let timer = Instant::now();
-                for _ in 0..n_runs {
-                    self.timed_request(&timer, &request, &mut stats_collector);
-                }
-            }
-            ConcurrenyLevel::Concurrent(_level) => {
-                todo!("use rayon");
+        // Trigger un-timed requests, possibly to populate a cache or similiar
+        info!("Warming up");
+        for _ in 0..self.config.warmup_runs() {
+            if let Err(error) = request_builder.try_clone().unwrap().send() {
+                error!("Warm up failed: {:?}", error);
+                return None;
             }
         }
 
-        stats_collector.collect()
+        info!(
+            "Starting measurement of {} samples from {} (concurrency level {})",
+            n_runs,
+            self.config.url,
+            1 // TODO
+        );
+
+        match self.config.concurrency_level() {
+            ConcurrenyLevel::Sequential => {
+                collect_samples(request_builder, n_runs, du);
+            }
+            ConcurrenyLevel::Concurrent(n_threads) => {
+                // TODO: should we divide n-runs?
+                let mut stats_by_thread = Vec::with_capacity(n_threads);
+                // NOTE: cannot use rayon due to unsatisfied trait bounds
+                for _ in 0..n_threads.max(1) {
+                    info!("started new thread");
+
+                    let request_builder = request_builder.try_clone().unwrap();
+                    let duration_scale = du.clone();
+
+                    let sampler = std::thread::spawn(move || {
+                        collect_samples(request_builder, n_runs, duration_scale)
+                    });
+
+                    stats_by_thread.push(sampler);
+                }
+
+                for sampler in stats_by_thread {
+                    let stats = sampler.join().unwrap().collect();
+                    // TODO: merge stats, careful with max time etc
+                }
+
+                // TODO: what to do with the timeseries graph?
+            }
+        }
+
+        None
+        // let guard = stats_collector.lock().unwrap();
+        // guard.collect()
     }
 }
